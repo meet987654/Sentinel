904334ea95ed4b14cdbc94ebb3375627b6f28bb5
Meet <meetprajapatid0207@gmail.com>
2026-09-24 13:47:19 +0530

    fix:consumer fie updated

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -6,7 +6,7 @@ export function UserProfile({ user }: { user: UserResponse }) {
   return (
     <div className="user-profile">
       <span>User Email:</span>
-      <p>{user.email}</p>
+      <p>{user.user_email}</p>
     </div>
   );
 }
